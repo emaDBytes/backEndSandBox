11d1127f9ee9c3a8adf0fee5c3d3df3cdcc75dc6
emaDBytes <158591808+emaDBytes@users.noreply.github.com>
2024-09-07 16:56:01 +0300

    playing with endpoints

--- HelloTest/src/main/java/fi/haagahelia/course/web/HelloController.java
+++ HelloTest/src/main/java/fi/haagahelia/course/web/HelloController.java
@@ -4,12 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-	@Controller
+@Controller
 public class HelloController {
 
     @RequestMapping("/")
     public @ResponseBody String greeting() {
         return "Hello World";
+    }
+
+    @RequestMapping("/index")
+    public @ResponseBody String mainPage() {
+        return "This is the main page";
+    }
+
+    @RequestMapping("/contact")
+    public @ResponseBody String contactUs() {
+        return "This is the contact page";
     }
 
 }
