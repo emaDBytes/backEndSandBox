59fb6d7de687f2dafee27abb94a39a1253e5a6a0
emaDBytes <158591808+emaDBytes@users.noreply.github.com>
2024-09-16 12:51:55 +0300

    Some unknown changes.

---
+++ StudentList/src/main/java/fi/haagahelia/course/domain/StudentRepository.java
@@ -0,0 +1,9 @@
+package fi.haagahelia.course.domain;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface StudentRepository extends CrudRepository<Student, Long> {
+
+}
