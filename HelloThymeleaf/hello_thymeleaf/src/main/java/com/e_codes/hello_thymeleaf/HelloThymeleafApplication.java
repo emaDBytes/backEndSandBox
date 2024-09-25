864ddccea554a6c44ec69e7efad3a5e02f4b5ee7
emaDBytes <158591808+emaDBytes@users.noreply.github.com>
2024-09-25 16:28:37 +0300

    HelloThymeleaf initiated.

---
+++ HelloThymeleaf/hello_thymeleaf/src/main/java/com/e_codes/hello_thymeleaf/HelloThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.e_codes.hello_thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloThymeleafApplication.class, args);
+	}
+
+}
---
+++ HelloThymeleaf/hello_thymeleaf/src/test/java/com/e_codes/hello_thymeleaf/HelloThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_codes.hello_thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
