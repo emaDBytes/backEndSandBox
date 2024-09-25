25dc7deb8290175368c3a15be2fc9fca1c48ceef
emaDBytes <158591808+emaDBytes@users.noreply.github.com>
2024-09-25 17:37:46 +0300

    A simple hello app created.
    It's a Thymeleaf template which shows welcome message if age is greater than 18.

--- HelloThymeleaf/hello_thymeleaf/src/main/java/com/e_codes/hello_thymeleaf/HelloThymeleafController.java
+++ HelloThymeleaf/hello_thymeleaf/src/main/java/com/e_codes/hello_thymeleaf/HelloThymeleafController.java
@@ -2,18 +2,24 @@ package com.e_codes.hello_thymeleaf;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloThymeleafController {
 
+    // This method handles GET requests to the /hello path.
+    // It accepts two query parameters: "name" and "age".
+    // These parameters are mapped using the @RequestParam annotation.
     @GetMapping("/hello")
-    public String sayHello(@RequestParam String name, @RequestParam int age, Module modle) {
+    public String sayHello(@RequestParam String name, @RequestParam int age, Model model) {
 
-        modle.addAttribute("name", name);
-        modle.addAttribute("age", age);
+        // Adding the "name" and "age" parameters to the model, 
+        // so they can be accessed in the Thymeleaf template.
+        model.addAttribute("name", name);
+        model.addAttribute("age", age);
 
+        // Return the name of the Thymeleaf template (hello.html) to be rendered.
         return "hello";
     }
 }
