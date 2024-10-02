218c543099ce8dbeea2dbc217c02a0278f13f76d
emaDBytes <158591808+emaDBytes@users.noreply.github.com>
2024-10-02 09:45:22 +0300

    HelloList project initiated

---
+++ HelloList/hellotolist/src/main/java/fi/haagahelia/hellotolist/HellotolistApplication.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.hellotolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellotolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellotolistApplication.class, args);
+	}
+
+}
---
+++ HelloList/hellotolist/src/test/java/fi/haagahelia/hellotolist/HellotolistApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.hellotolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellotolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
